c0fc5462ba8755c518442d37b2f1f1fa3c8eba2f
Toni Iltanen <toni.iltanen@codecontrol.fi>
2018-05-14 23:59:13 +0300

    Footer Completed Added recent events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,6 @@ module ApplicationHelper
   def get_twitter_feed
     client = Rails.application.config.twitter
     @tweets = client.user_timeline("Marko_80").take(2)
+    @events_footer = Event.where('date >= ?', DateTime.now).order(date: :asc, time: :asc).take(2)
   end
 end
