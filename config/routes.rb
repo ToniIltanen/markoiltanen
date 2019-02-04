ec5d92ffae3fd0bcbcc31165ef50d818a1e8543c
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-02-04 10:30:20 +0200

    small enhances added devise auth for admin added User model for auth fixed gallery encounters.count

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'home#main'
-
   match "*path", to: "application#catch_404", via: :all
 end
